1e23301f66404fa8a18b9384eed6422edbf2abb0
Austin Borden <austin.borden@gmail.com>
2025-03-17 10:45:35 +0700

    update: versions

--- clients/js/src/anchorIdls/gumball_guard.ts
+++ clients/js/src/anchorIdls/gumball_guard.ts
@@ -1,5 +1,5 @@
 export type GumballGuard = {
-  version: '0.2.1';
+  version: '0.2.2';
   name: 'gumball_guard';
   instructions: [
     {
@@ -1436,7 +1436,7 @@ export type GumballGuard = {
 };
 
 export const IDL: GumballGuard = {
-  version: '0.2.1',
+  version: '0.2.2',
   name: 'gumball_guard',
   instructions: [
     {
